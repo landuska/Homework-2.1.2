741a1113c2c1c163e17e98d01825d810da1c0a96
Landysh Khusaenova <landysh.kg@gmail.com>
2023-10-17 21:20:36 +0200

    added files .md, js.

---
+++ variable.js
@@ -0,0 +1,12 @@
+let a = 1;
+let myWord = "world";
+
+console.log (typeof(a));
+console.log (typeof(String(a)));
+console.log (typeof(a+''));
+console.log (typeof(myWord));
+console.log (typeof(Number(myWord)));
+console.log (typeof(+myWord));
+console.log (typeof(parseInt(myWord)));
+console.log (typeof(Boolean(a)));
+console.log (typeof(!!a));
