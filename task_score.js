c0d5e840c9d865828be46f4d54479bab0e1e394c
landuska <44550742+landuska@users.noreply.github.com>
2023-10-22 14:53:40 +0200

    Homework 2.1.3_Update task_score.js

--- task_score.js
+++ task_score.js
@@ -1,18 +1,24 @@
-let scoreVvedenie = [20, 30, 10];
+let scoreVvedenie = [20, 57, 10];
 let scoreGit = [50, 15, 7];
-let scoreJs = [25, 15, 10];
-let averageScore;
+let scoreJs = [25, 15, 12];
 let Username = 'Landysh';
 let Usersurname = 'Khusaenova';
 
-let sumScoreVvedenie = scoreVvedenie.reduce((sum, current) => sum + current, 0);
-let sumScoreGit = scoreGit.reduce((sum, current) => sum + current, 0);
-let sumScoreJs = scoreJs.reduce((sum, current) => sum + current, 0);
-averageScore = (sumScoreVvedenie + sumScoreGit + sumScoreJs) / 3;
-//console.log(sumScoreGit);
-//console.log(sumScoreVvedenie);
-//console.log(sumScoreJs);
-console.log(`Средний балл студента/студентки ${Username = Username.slice(0,1)}.${Usersurname}: ${Math.ceil(averageScore)}`);
+let sumAverageScoreVvedenie = (scoreVvedenie.reduce((sum, current) => sum + current, 0)) / scoreVvedenie.length;
+let sumAverageScoreGit = (scoreGit.reduce((sum, current) => sum + current, 0)) / scoreGit.length;
+let sumAverageScoreJs = (scoreJs.reduce((sum, current) => sum + current, 0)) / scoreJs.length;
+
+let averageScore = []; 
+averageScore.push(sumAverageScoreVvedenie, sumAverageScoreGit, sumAverageScoreJs);
+let averageScoreUser = (averageScore.reduce((sum, current) => sum + current, 0)) / averageScore.length;
+
+// console.log(sumAverageScoreGit);
+// console.log(sumAverageScoreVvedenie);
+// console.log(sumAverageScoreJs);
+// console.log(averageScore)
+
+console.log(`Средний балл студента/студентки ${Username = Username.slice(0,1)}.${Usersurname}: ${Math.ceil(averageScoreUser)}`);
+
 
 
 
